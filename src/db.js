4f061e5e1dce3050975c9c27c188fc530d1c706c
hayatshin <82467520+hayatshin@users.noreply.github.com>
2022-06-26 03:47:35 +0900

    db_url

--- src/db.js
+++ src/db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
+require("dotenv").config();
 
-mongoose.connect("mongodb://127.0.0.1:27017/praticeguitar", {
+mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
