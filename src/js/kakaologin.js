eff77eda9704af228956c79de00e9c54ec29b9fa
hayatshin <82467520+hayatshin@users.noreply.github.com>
2022-06-26 16:40:51 +0900

    kakao login redirect uri

--- src/js/kakaologin.js
+++ src/js/kakaologin.js
@@ -2,23 +2,27 @@ const KakaoLogin = document.querySelector("#KakaoLogin");
 const KakaoLogout = document.querySelector("#KakaoLogout");
 
 const handleKakaoLogin = () => {
-  if (!window.Kakao.isInitialized()) {
+  console.log(Kakao.isInitialized());
+  if (!Kakao.isInitialized()) {
     window.Kakao.init("0ebf7b9d9ab2ab6fea8c1523158ba0fe");
     window.Kakao.Auth.login({
       scope: "profile_nickname, profile_image, account_email",
       success: function (authObj) {
-        console.log(authObj);
         window.Kakao.API.request({
           url: "/v2/user/me",
           success: function (res) {
-            console.log(res);
+            alert("로그인 성공");
           },
-        });
-        window.Kakao.Auth.authorize({
-          redirectUri: "https://practiceguitar.herokuapp.com/video",
-        });
+        }).then(
+          (window.location.href = "https://practiceguitar.herokuapp.com/video")
+        );
+      },
+      fail: function (error) {
+        console.log(error);
       },
     });
+  } else {
+    window.location.href = "https://practiceguitar.herokuapp.com/video";
   }
 };
 
